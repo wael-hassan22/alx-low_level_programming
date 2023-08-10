633f718c3a795e761e761b6060eccd835981e70c
wael <wsaadalnoor@gmail.com>
2023-08-10 10:08:36 -0700

    added 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+/**
+ * The Main function prints :Programming is like building a multilingual puzzle
+ * and return 0
+*/
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

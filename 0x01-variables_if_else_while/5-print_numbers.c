58c5d11457e3eb037ed1a4888ad88df32a1a04da
wael <wsaadalnoor@gmail.com>
2023-08-12 06:56:04 +0200

    fixed betty style

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all single digit numbers of base 10 
+ * main - prints all single digit numbers of base 10
  * starting from 0, followed by a new line
  * Return: reuturn 0
  */
